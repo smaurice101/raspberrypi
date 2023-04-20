e0e6001d92ad44ca7834036aaaf879e718526547
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2023-04-20 14:26:49 -0400

    Add files via upload

--- tmux-shell-script/tmux.sh
+++ tmux-shell-script/tmux.sh
@@ -3,26 +3,24 @@
 
 ########################### START ZOOKEEPER and KAFKA
  export KAFKA_HEAP_OPTS="-Xmx512M -Xms512M"
- export  userbasedir='/media/pi/sebusb'
+ export  userbasedir='/mnt/usb'
  MYIP=$(ip route get 8.8.8.8 | awk '{ print $7; exit }')
  export MYIP
  
-sudo rm $userbasedir/kafka/kafka-logs1/meta.properties
 
  sudo kill -9 $(sudo lsof -i:9092 -t) 2> /dev/null
-
- sleep 10
+ sudo kill -9 $(sudo lsof -i:2181 -t) 2> /dev/null
+ 
+ sleep 5
 
  tmux new -d -s zookeeper
- tmux send-keys -t zookeeper 'cd $userbasedir/zookeeper/kafka/bin' ENTER
- tmux send-keys -t zookeeper 'sudo ./zookeeper-server-start.sh ../config/zookeeper.properties' ENTER
+ tmux send-keys -t zookeeper 'sudo /home/pi/Kafka/kafka_2.13-3.0.0/bin/zookeeper-server-start.sh /home/pi/Kafka/kafka_2.13-3.0.0/config/zookeeper.properties' ENTER
 
 
- sleep 10
+ sleep 15
 
  tmux new -d -s kafka 
- tmux send-keys -t kafka 'cd $userbasedir/zookeeper/kafka/bin' ENTER
- tmux send-keys -t kafka 'sudo ./kafka-server-start.sh ../config/server.properties' ENTER
+ tmux send-keys -t kafka 'sudo /home/pi/Kafka/kafka_2.13-3.0.0/bin/kafka-server-start.sh /home/pi/Kafka/kafka_2.13-3.0.0/config/server.properties' ENTER
 
 sleep 45
  ########################## SETUP VIPER/HPDE/VIPERVIZ Binaries For Transactional Machine Learning 
@@ -32,9 +30,9 @@ sleep 45
   
  tmux new -d -s produce-iot-data-viper-8000 
  tmux send-keys -t produce-iot-data-viper-8000 'cd $userbasedir/viper' ENTER
- tmux send-keys -t produce-iot-data-viper-8000 'sudo $userbasedir/viper/viper-linux-arm 127.0.0.1 8000' ENTER
+ tmux send-keys -t produce-iot-data-viper-8000 'sudo $userbasedir/viper/viper-linux-arm64 127.0.0.1 8000' ENTER
 
- sleep 3
+ sleep 15
 # STEP 1b: RUN PYTHON Script 
  tmux new -d -s produce-iot-data-python-8000 
  tmux send-keys -t produce-iot-data-python-8000 'cd $userbasedir/IotSolution' ENTER
@@ -44,13 +42,13 @@ sleep 45
 # STEP 2a: RUN VIPER Binary
  tmux new -d -s preprocess-data-viper-8001
  tmux send-keys -t preprocess-data-viper-8001 'cd $userbasedir/viper' ENTER
- tmux send-keys -t preprocess-data-viper-8001 'sudo $userbasedir/viper/viper-linux-arm 127.0.0.1 8001' ENTER
+ tmux send-keys -t preprocess-data-viper-8001 'sudo $userbasedir/viper/viper-linux-arm64 127.0.0.1 8001' ENTER
 
  tmux new -d -s preprocess2-data-viper-8002
  tmux send-keys -t preprocess2-data-viper-8002 'cd $userbasedir/viper' ENTER 
- tmux send-keys -t preprocess2-data-viper-8002 'sudo $userbasedir/viper/viper-linux-arm 127.0.0.1 8002' ENTER
+ tmux send-keys -t preprocess2-data-viper-8002 'sudo $userbasedir/viper/viper-linux-arm64 127.0.0.1 8002' ENTER
  
-sleep 3 
+sleep 15
 
 # STEP 2b: RUN PYTHON Script  
  tmux new -d -s preprocess-data-python-8001
@@ -66,5 +64,5 @@ sleep 3
 # STEP 5: START Visualization Viperviz 
  tmux new -d -s visualization-viperviz-9005 
  tmux send-keys -t visualization-viperviz-9005 'cd $userbasedir/viperviz' ENTER
- tmux send-keys -t visualization-viperviz-9005 'sudo $userbasedir/viperviz/viperviz-linux-arm $MYIP 9005' ENTER
+ tmux send-keys -t visualization-viperviz-9005 'sudo $userbasedir/viperviz/viperviz-linux-arm64 $MYIP 9005' ENTER
  
