8440161ce9846a7b91816b969292dcd4083f6785
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2023-05-09 15:06:58 -0400

    Update tmux.sh

--- tmux-shell-script/tmux.sh
+++ tmux-shell-script/tmux.sh
@@ -6,6 +6,7 @@
  export  userbasedir='/mnt/usb'
  MYIP=$(ip route get 8.8.8.8 | awk '{ print $7; exit }')
  export MYIP
+ CHIP = "arm"
  
 # sudo mount -o remount,rw /partition/identifier $userbasedir
 
@@ -15,15 +16,15 @@
  sleep 5
 
  tmux new -d -s zookeeper
- tmux send-keys -t zookeeper 'cd /home/pi/Kafka/kafka_2.13-3.0.0/bin' ENTER
- tmux send-keys -t zookeeper './zookeeper-server-start.sh /home/pi/Kafka/kafka_2.13-3.0.0/config/zookeeper.properties' ENTER
+ tmux send-keys -t zookeeper 'cd $userbasedir/Kafka/kafka_2.13-3.0.0/bin' ENTER
+ tmux send-keys -t zookeeper './zookeeper-server-start.sh $userbasedir/Kafka/kafka_2.13-3.0.0/config/zookeeper.properties' ENTER
 
 
  sleep 20
 
  tmux new -d -s kafka 
  tmux send-keys -t kafka 'cd /home/pi/Kafka/kafka_2.13-3.0.0/bin' ENTER
- tmux send-keys -t kafka './kafka-server-start.sh /home/pi/Kafka/kafka_2.13-3.0.0/config/server.properties' ENTER
+ tmux send-keys -t kafka './kafka-server-start.sh $userbasedir/Kafka/kafka_2.13-3.0.0/config/server.properties' ENTER
 
 sleep 45
  ########################## SETUP VIPER/HPDE/VIPERVIZ Binaries For Transactional Machine Learning 
@@ -32,8 +33,8 @@ sleep 45
 # STEP 1a: RUN VIPER Binary
   
  tmux new -d -s produce-iot-data-viper-8000 
- tmux send-keys -t produce-iot-data-viper-8000 'cd $userbasedir/viper' ENTER
- tmux send-keys -t produce-iot-data-viper-8000 'sudo $userbasedir/viper/viper-linux-arm64 127.0.0.1 8000' ENTER
+ tmux send-keys -t produce-iot-data-viper-8000 'cd $userbasedir/Viper' ENTER
+ tmux send-keys -t produce-iot-data-viper-8000 'sudo $userbasedir/Viper/viper-linux-$CHIP 127.0.0.1 8000' ENTER
 
  sleep 25
 # STEP 1b: RUN PYTHON Script 
@@ -44,12 +45,12 @@ sleep 45
 # STEP 2: Preprocess Data from Kafka
 # STEP 2a: RUN VIPER Binary
  tmux new -d -s preprocess-data-viper-8001
- tmux send-keys -t preprocess-data-viper-8001 'cd $userbasedir/viper' ENTER
- tmux send-keys -t preprocess-data-viper-8001 'sudo $userbasedir/viper/viper-linux-arm64 127.0.0.1 8001' ENTER
+ tmux send-keys -t preprocess-data-viper-8001 'cd $userbasedir/Viper' ENTER
+ tmux send-keys -t preprocess-data-viper-8001 'sudo $userbasedir/Viper/viper-linux-$CHIP 127.0.0.1 8001' ENTER
 
  tmux new -d -s preprocess2-data-viper-8002
- tmux send-keys -t preprocess2-data-viper-8002 'cd $userbasedir/viper' ENTER 
- tmux send-keys -t preprocess2-data-viper-8002 'sudo $userbasedir/viper/viper-linux-arm64 127.0.0.1 8002' ENTER
+ tmux send-keys -t preprocess2-data-viper-8002 'cd $userbasedir/Viper' ENTER 
+ tmux send-keys -t preprocess2-data-viper-8002 'sudo $userbasedir/Viper/viper-linux-$CHIP 127.0.0.1 8002' ENTER
  
 sleep 25
 
@@ -66,6 +67,6 @@ sleep 25
 
 # STEP 5: START Visualization Viperviz 
  tmux new -d -s visualization-viperviz-9005 
- tmux send-keys -t visualization-viperviz-9005 'cd $userbasedir/viperviz' ENTER
- tmux send-keys -t visualization-viperviz-9005 'sudo $userbasedir/viperviz/viperviz-linux-arm64 $MYIP 9005' ENTER
+ tmux send-keys -t visualization-viperviz-9005 'cd $userbasedir/Viperviz' ENTER
+ tmux send-keys -t visualization-viperviz-9005 'sudo $userbasedir/Viperviz/viperviz-linux-$CHIP $MYIP 9005' ENTER
  
