bd7d2f6a6b766cab6ed4ca557436e44e1ec61d7a
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-07-26 10:57:54 -0400

    Add files via upload

---
+++ tml-airflow/dags/tml-dag.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+from datetime import datetime
+
+#Define default arguments
+default_args = {
+ 'owner': 'Otics Advanced Analytics',
+ 'start_date': datetime (2023, 9, 29),
+ 'retries': 1,
+}
+
+# Instantiate your DAG
+dag = DAG ('tml_iot_solution_dag', default_args=default_args, schedule=None)
+
+# Define tasks
+def task1():
+ print ("Executing Task 1")
+
+def task2():
+ print ("Executing Task 2")
+
+tml_init_task_start = BashOperator(
+    task_id='start',
+    bash_command='date'
+)
+
+tml_produce_data = PythonOperator(
+ task_id='tml_produce_data',
+ python_callable=task1,
+ dag=dag,
+)
+task_2 = PythonOperator(
+ task_id='task_2',
+ python_callable=task2,
+ dag=dag,
+)
+
+#task_start >> [task_get_users, task_get_posts, task_get_comments, task_get_todos]
+
+# Set task dependencies
+tml_init_task_start >> [tml_produce_data, task_2]
