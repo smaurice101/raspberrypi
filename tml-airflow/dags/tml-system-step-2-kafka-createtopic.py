198e6a1cf51b6b197e6d64710a692ddf86c20c7d
smaurice101 <support@otics.ca>
2024-07-31 16:25:29 +0000

    commits

---
+++ tml-airflow/dags/tml-system-step-2-kafka-createtopic.py
@@ -0,0 +1,69 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+from datetime import datetime
+
+#Define default arguments
+default_args = {
+ 'owner': 'Sebastian Maurice',
+ 'start_date': datetime (2024, 6, 29),
+ 'retries': 1,
+}
+
+# Instantiate your DAG
+dag = DAG (dag_id="tml_system_step_2_kafka_createtopic_dag", default_args=default_args, tags=["tml-system-step-2-kafka-createtopic"], schedule=None)
+
+def setupkafkatopic(topicname):
+     # Set personal data
+      companyname="OTICS"
+      myname="Sebastian"
+      myemail="Sebastian.Maurice"
+      mylocation="Toronto"
+
+      # Replication factor for Kafka redundancy
+      replication=1
+      # Number of partitions for joined topic
+      numpartitions=1
+      # Enable SSL/TLS communication with Kafka
+      enabletls=1
+      # If brokerhost is empty then this function will use the brokerhost address in your
+      # VIPER.ENV in the field 'KAFKA_CONNECT_BOOTSTRAP_SERVERS'
+      brokerhost=''
+      # If this is -999 then this function uses the port address for Kafka in VIPER.ENV in the
+      # field 'KAFKA_CONNECT_BOOTSTRAP_SERVERS'
+      brokerport=-999
+      # If you are using a reverse proxy to reach VIPER then you can put it here - otherwise if
+      # empty then no reverse proxy is being used
+      microserviceid=''
+
+
+      #############################################################################################################
+      #                         CREATE TOPIC TO STORE TRAINED PARAMS FROM ALGORITHM  
+      
+      producetotopic=topicname
+
+      description="Topic to store the trained machine learning parameters"
+      result=maadstml.vipercreatetopic(VIPERTOKEN,VIPERHOST,VIPERPORT,producetotopic,companyname,
+                                     myname,myemail,mylocation,description,enabletls,
+                                     brokerhost,brokerport,numpartitions,replication,
+                                     microserviceid='')
+      # Load the JSON array in variable y
+      print("Result=",result)
+      try:
+         y = json.loads(result,strict='False')
+      except Exception as e:
+         y = json.loads(result)
+
+
+      for p in y:  # Loop through the JSON ang grab the topic and producerids
+         pid=p['ProducerId']
+         tn=p['Topic']
+         
+      return tn,pid
+
+topic,pid = PythonOperator(
+ task_id='step-2-kafka-createtopic',
+ python_callable=setupkafkatopic,
+ dag=dag,
+)
