3e20ff5506e145e9ee9ddc7adbb1f5604173c177
smaurice101 <support@otics.ca>
2024-08-17 14:52:40 +0000

    commit

--- tml-airflow/dags/tml-client-RESTAPI-step-3-kafka-producetotopic.py
+++ tml-airflow/dags/tml-client-RESTAPI-step-3-kafka-producetotopic.py
@@ -5,7 +5,14 @@ sys.dont_write_bytecode = True
  
 # defining the api-endpoint
 rest_port = "9001"  # <<< ***** Change Port to match the Server Rest_PORT
-API_ENDPOINT = "http://localhost:{}/api/api_post.php".format(rest_port)
+
+# Modify the apiroute: jsondataline, or jsondataarray
+# 1. jsondataline: You can send One Json message at a time
+# 1. jsondatarray: You can send a Json array 
+
+apiroute = "jsondataline"
+
+API_ENDPOINT = "http://localhost:{}/{}".format(rest_port,apiroute)
  
 def send_tml_data(data): 
   # data to be sent to api
@@ -18,9 +25,10 @@ def send_tml_data(data):
     
 
 def start():
+    
       ######### Modify datajson as you need ##############  
       try:  
-        datajson = {"Type": "data1 data 2", "Value": "value 1"}      
+        datajson = {"Type": "data1 data 2", "Value": "value 1"}   # << ** This json would normal come from some device, or you read from a file   
         ret = send_tml_data(datajson)
         print(ret)  
       except Exception as e:
