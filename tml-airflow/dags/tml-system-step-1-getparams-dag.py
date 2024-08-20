3d8c9bbec20bcc3fcba278474699d6bc5fb3def2
smaurice101 <support@otics.ca>
2024-08-20 21:37:08 +0000

    commit

--- tml-airflow/dags/tml-system-step-1-getparams-dag.py
+++ tml-airflow/dags/tml-system-step-1-getparams-dag.py
@@ -58,7 +58,7 @@ default_args = {
  'SSL_CLIENT_KEY_FILE' : 'client.key.pem', 
  'SSL_SERVER_CERT_FILE' : 'server.cer.pem',  
  'KUBERNETES' : 0,
- 'solutionname': '_mysolution_',   # <<< *** DO NOT MODIFY = THIS WILL BE AUTOMATICALLY UPDATED
+ 'solutionname': '_mysolution_',   # <<< *** DO NOT MODIFY - THIS WILL BE AUTOMATICALLY UPDATED
  'solutiontitle': 'My Solution Title', # <<< *** Provide a descriptive title for your solution
  'description': 'This is an awesome real-time solution built by TSS',   # <<< *** Provide a description of your solution
  'retries': 1,
