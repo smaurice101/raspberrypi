4c40db452029d5abfc201f4813d692ad7834c929
root <root@smaurice>
2024-08-22 14:30:54 +0000

    Entry from tml-read-gRPC-step-3-kafka-producetotopic-dag.py

--- tml-airflow/dags/solution_template_processing_ml_ai_dag_restapi.py
+++ tml-airflow/dags/solution_template_processing_ml_ai_dag_restapi.py
@@ -43,8 +43,8 @@ with DAG(
 # STEP 3: Produce data to topic        
   sensor_C = ExternalTaskSensor(
       task_id="solution_task_producetotopic",
-      external_dag_id="tml_localfile_step_3_kafka_producetotopic_dag",
-      external_task_id="readdata",
+      external_dag_id="tml_RESTAPI_step_3_kafka_producetotopic_dag",
+      external_task_id="gettmlsystemsparams",
   )
 # STEP 4: Preprocess the data        
   sensor_D = ExternalTaskSensor(
@@ -52,22 +52,41 @@ with DAG(
       external_dag_id="tml_system_step_4_kafka_preprocess_dag",
       external_task_id="processtransactiondata",
   )
+# STEP 5: ML        
   sensor_E = ExternalTaskSensor(
+      task_id="solution_task_ml",
+      external_dag_id="tml_system_step_5_kafka_machine_learning_dag",
+      external_task_id="performSupervisedMachineLearning",
+  )
+# STEP 6: Predictions        
+  sensor_F = ExternalTaskSensor(
+      task_id="solution_task_prediction",
+      external_dag_id="tml_system_step_6_kafka_predictions_dag",
+      external_task_id="performPredictions",
+  )    
+# STEP 7: Visualization        
+  sensor_G = ExternalTaskSensor(
       task_id="solution_task_visualization",
       external_dag_id="tml_system_step_7_kafka_visualization_dag",
       external_task_id="startstreamingengine",
   )
 # STEP 8: Containerize the solution        
-  sensor_F = ExternalTaskSensor(
+  sensor_H = ExternalTaskSensor(
       task_id="solution_task_containerize",
       external_dag_id="tml_system_step_8_deploy_solution_to_docker_dag",
       external_task_id="dockerit",
   )
+# STEP 9: PrivateGPT      
+  sensor_I = ExternalTaskSensor(
+      task_id="solution_task_ai",
+      external_dag_id="tml_system_step_9_privategpt_qdrant_dag",
+      external_task_id="startprivategpt",
+  )            
 # STEP 10: Document the solution
-  sensor_G = ExternalTaskSensor(
+  sensor_J = ExternalTaskSensor(
       task_id="solution_task_document",
       external_dag_id="tml_system_step_10_documentation_dag",
       external_task_id="generatedoc",
   )
 
-  start_task >> sensor_A >> sensor_B >> sensor_C >> sensor_D >> sensor_E >> sensor_F >> sensor_G
+  start_task >> sensor_A >> sensor_B >> sensor_C >> sensor_D >> [sensor_E, sensor_F, sensor_I] >> sensor_G >> sensor_H >> sensor_J
