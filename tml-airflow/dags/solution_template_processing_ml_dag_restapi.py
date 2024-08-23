ee79099365fcca566c68921f10f05748edd93136
root <root@smaurice>
2024-08-23 17:55:25 +0000

    Entry from tml-system-step-7-kafka-visualization-dag-iotfailuredetection.py

--- tml-airflow/dags/solution_template_processing_ml_dag_restapi.py
+++ tml-airflow/dags/solution_template_processing_ml_dag_restapi.py
@@ -43,7 +43,7 @@ with DAG(
 # STEP 3: Produce data to topic        
   sensor_C = ExternalTaskSensor(
       task_id="solution_task_producetotopic",
-      external_dag_id="tml_RESTAPI_step_3_kafka_producetotopic_dag",
+      external_dag_id="tml_read_RESTAPI_step_3_kafka_producetotopic_dag",
       external_task_id="gettmlsystemsparams",
   )
 # STEP 4: Preprocess the data        
