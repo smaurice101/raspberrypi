6181b9627c063c20a9c77dd33de2afccbbea2174
root <root@smaurice>
2024-08-23 19:54:07 +0000

    Entry from tml-system-step-7-kafka-visualization-dag.py

---
+++ tml-airflow/dags/tml-solutions/mytmltestsolution2/tsslogging.py
@@ -0,0 +1,36 @@
+# TSS Logging
+
+import datetime
+from datetime import timezone 
+from git import Repo
+
+def getrepo(filename='/tmux/reponame.txt'):
+  with open(filename, "r") as file1:
+    # Reading from a file
+    repo=file1.read()
+    repo=repo.rstrip()
+    
+  return repo
+
+def git_push(repopath,message,sname):
+    try:
+        repo = Repo(repopath)
+        repo.git.add(update=True)
+        repo.index.commit(message)
+        origin = repo.remote(name=sname)
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')    
+
+def tsslogit(message,mtype="INFO"):
+  repo=""    
+  now = datetime.datetime.now(timezone.utc)
+  dbuf = "[INFO " + now.strftime("%Y-%m-%d_%H:%M:%S") + "]"
+  
+  repo=getrepo()  
+
+    #[INFO 2024-08-18_19:24:06]
+  with open("/{}/tml-airflow/logs/logs.txt".format(repo), "a") as file1:
+    # Reading from a file
+    dbuf = "[{} {}]".format(mtype,now.strftime("%Y-%m-%d_%H:%M:%S"))
+    file1.write("{} {}\n".format(dbuf,message))
