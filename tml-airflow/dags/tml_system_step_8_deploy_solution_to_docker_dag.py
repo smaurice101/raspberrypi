68a07ced08193e8f50a56b2984ae21d10dc8c1f5
root <root@smaurice>
2024-08-31 20:41:49 +0000

    Entry from tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py

--- tml-airflow/dags/tml_system_step_8_deploy_solution_to_docker_dag.py
+++ tml-airflow/dags/tml_system_step_8_deploy_solution_to_docker_dag.py
@@ -34,7 +34,7 @@ def dockerit(**context):
        repo=tsslogging.getrepo()    
        tsslogging.tsslogit("Docker DAG in {}".format(os.path.basename(__file__)), "INFO" )                     
        tsslogging.git_push("/{}".format(repo),"Entry from {}".format(os.path.basename(__file__)),"origin")            
-       sname = context['ti'].xcom_pull(task_ids='solution_task_getparams',key="solutionname")
+       sname = context['ti'].xcom_pull(task_ids='step_1_solution_task_getparams',key="solutionname")
        if 'CHIP' in os.environ:
           chip = os.environ['CHIP']
        else:
