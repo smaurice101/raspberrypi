716d78d18f68a9f358a55593fec7f2d41e4bffc4
smaurice101 <smaurice101@email>
2024-09-08 02:59:57 +0000

    Committing document for raspberrypi GOTO https://raspberrypi.readthedocs.io for details.

--- tml-airflow/dags/tml-solutions/myawesometmlsolution/tml_read_RESTAPI_step_3_kafka_producetotopic_dag-myawesometmlsolution.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolution/tml_read_RESTAPI_step_3_kafka_producetotopic_dag-myawesometmlsolution.py
@@ -83,7 +83,7 @@ def gettmlsystemsparams(VIPERTOKEN,VIPERHOST,VIPERPORT):
         @app.route(rule='/jsondataline', methods=['POST'])
         def storejsondataline():
           jdata = request.get_json()
-          with open("/tmux/test.txt".format(sname), 'a', encoding='utf-8') as file: 
+          with open("/tmux/test.txt", 'a', encoding='utf-8') as file: 
                file.writelines("{},{}\n".format(app.config['VIPERHOST'],app.config['VIPERPORT']))
           readdata(jdata,app.config['VIPERTOKEN'],app.config['VIPERHOST'],app.config['VIPERPORT'])
           return "ok"
