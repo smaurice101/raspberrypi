6e7c34369dbd7cf6088eb498790633baeacfb89d
smaurice101 <smaurice101@email>
2024-09-08 03:09:23 +0000

    Committing document for raspberrypi GOTO https://raspberrypi.readthedocs.io for details.

--- tml-airflow/dags/tml-solutions/myawesometmlsolution/tml_read_RESTAPI_step_3_kafka_producetotopic_dag-myawesometmlsolution.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolution/tml_read_RESTAPI_step_3_kafka_producetotopic_dag-myawesometmlsolution.py
@@ -77,7 +77,7 @@ def gettmlsystemsparams(VIPERTOKEN,VIPERHOST,VIPERPORT):
         app = Flask(__name__)
                  
         app.config['VIPERTOKEN'] = os.environ['VIPERTOKEN']
-        app.config['VIPERHOST'] = os.environ['VIPERHOST']
+        app.config['VIPERHOST'] = "https://127.0.0.1"
         app.config['VIPERPORT'] = os.environ['VIPERPORT']
                 
                
