627d404e9567f405135ca5da45f4cf4636120cd5
smaurice101 <support@otics.ca>
2024-09-08 15:26:13 +0000

    commit

--- tml-airflow/dags/tml_system_step_10_documentation_dag.py
+++ tml-airflow/dags/tml_system_step_10_documentation_dag.py
@@ -178,7 +178,8 @@ def generatedoc(**context):
     PORT = ""
     IDENTIFIER = ""
     HTTPADDR = ""
-    CLIENTHOST = ""
+    FROMHOST = ""
+    TOHOST = ""    
     CLIENTPORT = ""
     
     PRODUCETYPE = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_PRODUCETYPE".format(sname))
@@ -186,14 +187,18 @@ def generatedoc(**context):
     PORT = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_PORT".format(sname))
     IDENTIFIER = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_IDENTIFIER".format(sname))
     HTTPADDR = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_HTTPADDR".format(sname))  
-    CLIENTHOST = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_CLIENTHOST".format(sname))  
+    FROMHOST = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_FROMHOST".format(sname))  
+    TOHOST = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_TOHOST".format(sname))  
+    
     CLIENTPORT = context['ti'].xcom_pull(task_ids='step_3_solution_task_producetotopic',key="{}_CLIENTPORT".format(sname))              
 
     subprocess.call(["sed", "-i", "-e",  "s/--PRODUCETYPE--/{}/g".format(PRODUCETYPE), "/{}/docs/source/details.rst".format(sname)])
     subprocess.call(["sed", "-i", "-e",  "s/--TOPIC--/{}/g".format(TOPIC), "/{}/docs/source/details.rst".format(sname)])
     doparse("/{}/docs/source/details.rst".format(sname), ["--PORT--;{}".format(PORT)])
     doparse("/{}/docs/source/details.rst".format(sname), ["--HTTPADDR--;{}".format(HTTPADDR)])
-    doparse("/{}/docs/source/details.rst".format(sname), ["--CLIENTHOST--;{}".format(CLIENTHOST)])
+    doparse("/{}/docs/source/details.rst".format(sname), ["--FROMHOST--;{}".format(FROMHOST)])
+    doparse("/{}/docs/source/details.rst".format(sname), ["--TOHOST--;{}".format(TOHOST)])
+    
     doparse("/{}/docs/source/details.rst".format(sname), ["--CLIENTPORT--;{}".format(CLIENTPORT)])
     
     subprocess.call(["sed", "-i", "-e",  "s/--IDENTIFIER--/{}/g".format(IDENTIFIER), "/{}/docs/source/details.rst".format(sname)])
