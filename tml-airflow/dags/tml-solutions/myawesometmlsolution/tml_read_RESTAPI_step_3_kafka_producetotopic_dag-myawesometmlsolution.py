76c3e9e4d4e16b4d1b67dc181df5561ada658a49
smaurice101 <smaurice101@email>
2024-09-08 01:21:12 +0000

    Committing document for raspberrypi GOTO https://raspberrypi.readthedocs.io for details.

--- tml-airflow/dags/tml-solutions/myawesometmlsolution/tml_read_RESTAPI_step_3_kafka_producetotopic_dag-myawesometmlsolution.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolution/tml_read_RESTAPI_step_3_kafka_producetotopic_dag-myawesometmlsolution.py
@@ -63,7 +63,7 @@ def producetokafka(value, tmlid, identifier,producerid,maintopic,substream,args)
      enabletls = int(args['enabletls'])
      identifier = args['identifier']
      print("VIPERPORT=",VIPERPORT,VIPERHOST)
-
+     VIPERPORT="1121"
      try:
         result=maadstml.viperproducetotopic(VIPERTOKEN,VIPERHOST,VIPERPORT,maintopic,producerid,enabletls,delay,'','', '',0,inputbuf,substream,
                                             topicid,identifier)
