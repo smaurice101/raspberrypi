c7c6d48369cd26acab50a2cbe31b75225cc8f6f1
smaurice101 <smaurice101@email>
2024-09-09 22:12:07 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/myawesometmlsolution-3f10/tml_read_gRPC_step_3_kafka_producetotopic_dag-myawesometmlsolution-3f10.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolution-3f10/tml_read_gRPC_step_3_kafka_producetotopic_dag-myawesometmlsolution-3f10.py
@@ -62,7 +62,7 @@ class TmlprotoService(pb2_grpc.TmlprotoServicer):
 
     # get the string from the incoming request
     message = request.message
-    print(message)
+    print({message})
     #readata(message)
     #result = f'Hello I am up and running received "{message}" message from you'
     #result = {'message': result, 'received': True}
