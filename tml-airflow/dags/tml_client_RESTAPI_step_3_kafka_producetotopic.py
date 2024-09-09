530f4c953140c17673370f25d03197b189ba7d6f
smaurice101 <support@otics.ca>
2024-09-09 23:01:05 +0000

    commit

--- tml-airflow/dags/tml_client_RESTAPI_step_3_kafka_producetotopic.py
+++ tml-airflow/dags/tml_client_RESTAPI_step_3_kafka_producetotopic.py
@@ -1,38 +1,71 @@
-import requests
-import sys
+ import requests
+ import sys
+ from datetime import datetime
+ import time
+ import json
 
-sys.dont_write_bytecode = True
- 
-# defining the api-endpoint
-rest_port = "9001"  # <<< ***** Change Port to match the Server Rest_PORT
+ sys.dont_write_bytecode = True
 
-# Modify the apiroute: jsondataline, or jsondataarray
-# 1. jsondataline: You can send One Json message at a time
-# 1. jsondatarray: You can send a Json array 
+ # defining the api-endpoint
+ rest_port = "9001"  # <<< ***** Change Port to match the Server Rest_PORT
+ httpaddr = "http:" # << Change to https or http
 
-apiroute = "jsondataline"
+ # Modify the apiroute: jsondataline, or jsondataarray
+ # 1. jsondataline: You can send One Json message at a time
+ # 1. jsondatarray: You can send a Json array
 
-API_ENDPOINT = "http://localhost:{}/{}".format(rest_port,apiroute)
- 
-def send_tml_data(data): 
-  # data to be sent to api
- 
-  # sending post request and saving response as response object
-  r = requests.post(url=API_ENDPOINT, data=data)
- 
-  # extracting response text
-  return r.text
-    
+ apiroute = "jsondataline"
 
-def start():
-    
-      ######### Modify datajson as you need ##############  
-      try:  
-        datajson = {"Type": "data1 data 2", "Value": "value 1"}   # << ** This json would normal come from some device, or you read from a file   
-        ret = send_tml_data(datajson)
-        print(ret)  
-      except Exception as e:
-        print("ERROR: ",e) 
-        
-if __name__ == '__main__':
-    start()
+ API_ENDPOINT = "{}//localhost:{}/{}".format(httpaddr,rest_port,apiroute)
+
+ def send_tml_data(data):
+   # data to be sent to api
+   headers = {'Content-type': 'application/json'}
+   r = requests.post(url=API_ENDPOINT, data=json.dumps(data), headers=headers)
+
+   # extracting response text
+   return r.text
+
+ def readdatafile(inputfile):
+
+   ##############################################################
+   # NOTE: You can send any "EXTERNAL" data through this API
+   # It is reading a localfile as an example
+   ############################################################
+
+   try:
+     file1 = open(inputfile, 'r')
+     print("Data Producing to Kafka Started:",datetime.now())
+   except Exception as e:
+     print("ERROR: Something went wrong ",e)
+     return
+   k = 0
+   while True:
+     line = file1.readline()
+     line = line.replace(";", " ")
+     print("line=",line)
+     # add lat/long/identifier
+     k = k + 1
+     try:
+       if line == "":
+         #break
+         file1.seek(0)
+         k=0
+         print("Reached End of File - Restarting")
+         print("Read End:",datetime.now())
+         continue
+       ret = send_tml_data(line)
+       print(ret)
+       # change time to speed up or slow down data
+       time.sleep(.5)
+     except Exception as e:
+       print(e)
+       time.sleep(.5)
+       pass
+
+ def start():
+       inputfile = "IoTDatasample.txt"
+       readdatafile(inputfile)
+
+ if __name__ == '__main__':
+     start()
