5309a5285805ff9126b1d3c4d290bf3441cb1020
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-09-16 09:37:09 -0400

    Add files via upload

---
+++ tml-airflow/dashboard/js/button.js
@@ -0,0 +1,20 @@
+//Multiple options dropdown
+//https://codepen.io/gatoledo1/pen/QWmpWjK
+
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let selectedOption = option.querySelector(".option-text").innerText;
+    sBtn_text.innerText = selectedOption;
+
+    optionMenu.classList.remove("active");
+  });
+});
