7098c090ce568636658b61a93583333369b28e86
smaurice101 <smaurice101@email>
2024-09-17 23:09:17 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
@@ -139,7 +139,7 @@ def consumetopicdata():
                   offset, brokerhost,brokerport,microserviceid,
                   topicid,rollbackoffsets,preprocesstype)
 
-#      print(result)
+      print(result)
       return result
 
 def gatherdataforprivategpt(result):
