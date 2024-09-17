05a8ef67b876e673b8f7c93d06d9bf81c9dfd67e
smaurice101 <smaurice101@email>
2024-09-17 01:21:41 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/cybersecuritysolutionwithprivategpt-3f10/tml_system_step_4_kafka_preprocess_dag-cybersecuritysolutionwithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritysolutionwithprivategpt-3f10/tml_system_step_4_kafka_preprocess_dag-cybersecuritysolutionwithprivategpt-3f10.py
@@ -36,7 +36,7 @@ default_args = {
   'tmlfilepath' : '', # leave blank
   'usemysql' : '1', # do not modify
   'streamstojoin' : '', # leave blank
-  'identifier' : 'IoT device performance and failures', # <<< ** Change as needed
+  'identifier' : 'Detect potential cyber attacks and monitor network', # <<< ** Change as needed
   'preprocesstypes' : 'anomprob,trend,avg', # <<< **** MAIN PREPROCESS TYPES CHNAGE AS NEEDED refer to https://tml-readthedocs.readthedocs.io/en/latest/
   'pathtotmlattrs' : 'oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a', # Change as needed     
   'jsoncriteria' : 'uid=metadata.dsn,filter:allrecords~\
