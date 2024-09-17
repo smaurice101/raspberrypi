f4b8aba28bb0f45cfe446935f3e20d1b70babac7
smaurice101 <smaurice101@email>
2024-09-17 01:26:58 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_2_kafka_createtopic_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_2_kafka_createtopic_dag-cybersecuritywithprivategpt-3f10.py
@@ -24,8 +24,8 @@ default_args = {
   'brokerhost' : '',  # <<< ********** Leave as is
   'brokerport' : '-999',  # <<< ********** Leave as is
   'microserviceid' : '',  # <<< ********** You change as needed
-  'raw_data_topic' : 'iot-raw-data', # Separate multiple topics with comma <<< ********** You change topic names as needed
-  'preprocess_data_topic' : 'iot-preprocess,iot-preprocess2', # Separate multiple topics with comma <<< ********** You change topic names as needed
+  'raw_data_topic' : 'cisco-network-mainstream', # Separate multiple topics with comma <<< ********** You change topic names as needed
+  'preprocess_data_topic' : 'cisco-network-preprocess', # Separate multiple topics with comma <<< ********** You change topic names as needed
   'ml_data_topic' : 'ml-data', # Separate multiple topics with comma <<< ********** You change topic names as needed
   'prediction_data_topic' : 'prediction-data', # Separate multiple topics with comma <<< ********** You change topic names as needed
   'pgpt_data_topic' : 'cisco-network-privategpt',  #  PrivateGPT will produce responses to this topic - change as  needed
