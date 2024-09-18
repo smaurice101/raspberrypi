cc7e3e96df1b43b474034376dc236197c3ae7416
smaurice101 <smaurice101@email>
2024-09-18 18:20:25 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
@@ -242,6 +242,7 @@ def gatherdataforprivategpt(result):
 def sendtoprivategpt(maindata):
 
    counter = 0   
+   maxc = 120
    pgptendpoint="/v1/completions"
    
    maintopic = default_args['pgpt_data_topic']
@@ -267,7 +268,7 @@ def sendtoprivategpt(maindata):
         else:
           counter += 1
           time.sleep(1)
-          if counter > 60:                
+          if counter > maxc:                
              startpgptcontainer()
              qdrantcontainer()
              counter = 0 
