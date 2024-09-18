c941ecba0482690b9f01842df7fa670904726703
smaurice101 <smaurice101@email>
2024-09-18 00:18:53 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml_system_step_9_privategpt_qdrant_dag.py
+++ tml-airflow/dags/tml_system_step_9_privategpt_qdrant_dag.py
@@ -186,6 +186,7 @@ def gatherdataforprivategpt(result):
          isin1 = False
          isin2 = False
          found=0
+         identarr=r['Identifier'].split("~")   
          if processtype != '' and attribute != '':
            processtype = processtype.lower()
            ptypearr = processtype.split(",")
@@ -199,7 +200,7 @@ def gatherdataforprivategpt(result):
              buf = r[jsonkeytogather]
              if buf != '':
                found=1
-               message = message  + buf + ', '
+               message = message  + "{} (Identifier={})".format(buf,identarr[0]) + ', '
          elif processtype != '' and attribute == '':
            processtype = processtype.lower()
            ptypearr = processtype.split(",")
@@ -208,7 +209,7 @@ def gatherdataforprivategpt(result):
              buf = r[jsonkeytogather]
              if buf != '':
                found=1
-               message = message  + buf + ', '
+               message = message  + "{} (Identifier={})".format(buf,identarr[0]) + ', '
          elif processtype == '' and attribute != '':
            attribute = attribute.lower()
            aar = attribute.split(",")
@@ -217,12 +218,12 @@ def gatherdataforprivategpt(result):
              buf = r[jsonkeytogather]
              if buf != '':
                found=1
-               message = message  + buf + ', '
+               message = message  + "{} (Identifier={})".format(buf,identarr[0]) + ', '
          else:
            buf = r[jsonkeytogather]
            if buf != '':
              found=1
-             message = message  + buf + ', '
+             message = message  + "{} (Identifier={})".format(buf,identarr[0]) + ', '
 
    if jsonkeytogather != 'Identifier' and found:
      message = "{}.  Data: {}.  {}".format(context,message,prompt)
