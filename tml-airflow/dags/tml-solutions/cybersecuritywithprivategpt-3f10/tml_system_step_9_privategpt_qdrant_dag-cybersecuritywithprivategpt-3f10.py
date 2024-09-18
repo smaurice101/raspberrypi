de7b36216dcf9fb7f4515463671475c98a569d98
smaurice101 <smaurice101@email>
2024-09-18 18:15:24 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
@@ -365,6 +365,7 @@ if __name__ == '__main__':
                sendtoprivategpt(maindata)
              time.sleep(1)
          except Exception as e:
+          print("error=",e)
           tsslogging.tsslogit("PrivateGPT Step 9 DAG in {} {}".format(os.path.basename(__file__),e), "ERROR" )
           tsslogging.git_push("/{}".format(repo),"Entry from {}".format(os.path.basename(__file__)),"origin")
           break
