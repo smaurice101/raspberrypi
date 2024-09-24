e2afeef00ca5c9f6e1f54a0a27ab7b49deddc62e
smaurice101 <smaurice101@email>
2024-09-24 23:53:54 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/iotsolution-3f10/solution_template_processing_dag-iotsolution-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolution-3f10/solution_template_processing_dag-iotsolution-3f10.py
@@ -98,4 +98,4 @@ with DAG(
       provide_context=True,      
   )
 
-  start_task >> sensor_A >> sensor_B >> start_task4 >> [sensor_C, sensor_D, sensor_E] >> start_task2 >> sensor_F >> start_task3  >> sensor_G
+  start_task >> sensor_A >> sensor_B >> start_task4 >> [sensor_C, sensor_D, sensor_D2, sensor_E] >> start_task2 >> sensor_F >> start_task3  >> sensor_G
