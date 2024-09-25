2efd8bab663a56d361311a26ad4fb10d22e179ed
smaurice101 <smaurice101@email>
2024-09-25 18:57:17 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/iotsolution-3f10/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag-iotsolution-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolution-3f10/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag-iotsolution-3f10.py
@@ -144,7 +144,9 @@ def startproducing(**context):
      fullpath="/{}/tml-airflow/dags/tml-solutions/{}/{}".format(repo,sname,os.path.basename(__file__))  
   else:
      fullpath="/{}/tml-airflow/dags/{}".format(repo,os.path.basename(__file__))  
-    
+  
+  return
+
   wn = windowname('produce',sname,sd)  
   subprocess.run(["tmux", "new", "-d", "-s", "{}".format(wn)])
   subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "cd /Viper-produce", "ENTER"])
