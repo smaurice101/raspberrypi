87f401ba2bd5365c7df88e32673ddca68903a842
smaurice101 <smaurice101@email>
2024-09-26 00:38:22 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml_system_step_1_getparams_dag.py
+++ tml-airflow/dags/tml_system_step_1_getparams_dag.py
@@ -130,7 +130,12 @@ def updateviperenv():
           cloudusername = os.environ['KAFKACLOUDUSERNAME']
     if 'KAFKACLOUDPASSWORD' in os.environ:
           cloudpassword = os.environ['KAFKACLOUDPASSWORD']
-                
+
+
+    if '127.0.0.1' in fault_args['brokerhost']:
+      cloudusername = ""
+      cloudpassword = ""
+        
     filepaths = ['/Viper-produce/viper.env','/Viper-preprocess/viper.env','/Viper-preprocess-pgpt/viper.env','/Viper-preprocess2/viper.env','/Viper-ml/viper.env','/Viper-predict/viper.env','/Viperviz/viper.env']
     for mainfile in filepaths:
      with open(mainfile, 'r', encoding='utf-8') as file: 
