8ada449793d60425b5be9d85490ca5ae8b621aad
smaurice101 <smaurice101@email>
2024-09-26 00:44:37 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/iotsolution-3f10/tml_system_step_1_getparams_dag-iotsolution-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolution-3f10/tml_system_step_1_getparams_dag-iotsolution-3f10.py
@@ -20,7 +20,7 @@ default_args = {
  'cloudusername' : '',  # <<<< --THIS WILL BE UPDATED FOR YOU IF USING KAFKA CLOUD WITH API KEY  - LEAVE BLANK
  'cloudpassword' : '',  # <<<< --THIS WILL BE UPDATED FOR YOU IF USING KAFKA CLOUD WITH API SECRET - LEAVE BLANK   
  'solutionname': 'iotsolution-3f10',   # <<< *** DO NOT MODIFY - THIS WILL BE AUTOMATICALLY UPDATED
- 'solutiontitle': 'My Solution Title', # <<< *** Provide a descriptive title for your solution
+ 'solutiontitle': 'IOT Monitoring Solution', # <<< *** Provide a descriptive title for your solution
  'solutionairflowport' : '-1', # << If -1, TSS will choose a free port randonly, or set this to a fixed number
  'solutionexternalport' : '-1', # << If -1, TSS will choose a free port randonly, or set this to a fixed number
  'solutionvipervizport' : '-1', # << If -1, TSS will choose a free port randonly, or set this to a fixed number   
