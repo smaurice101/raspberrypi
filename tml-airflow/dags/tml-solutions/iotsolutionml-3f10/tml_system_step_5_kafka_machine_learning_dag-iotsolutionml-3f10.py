b7e8fe05155e1260c0f2df7b228c830496265ee2
smaurice101 <smaurice101@email>
2024-09-27 16:46:57 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/iotsolutionml-3f10/tml_system_step_5_kafka_machine_learning_dag-iotsolutionml-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolutionml-3f10/tml_system_step_5_kafka_machine_learning_dag-iotsolutionml-3f10.py
@@ -40,8 +40,8 @@ default_args = {
   'partition_training' : '',  # leave blank
   'consumefrom' : '',  # leave blank
   'topicid' : '-1',  # leave as is
-  'fullpathtotrainingdata' : '/Viper-ml/viperlogs/logistic',  #  # <<< *** Change as needed - add name for foldername that stores the training datasets
-  'processlogic' : '',  # <<< *** Change as needed, i.e. classification_name=failure_prob:Voltage_preprocessed_AnomProb=55,n:Current_preprocessed_AnomProb=55,n
+  'fullpathtotrainingdata' : '/Viper-ml/viperlogs/iotlogistic',  #  # <<< *** Change as needed - add name for foldername that stores the training datasets
+  'processlogic' : 'classification_name=failure_prob:Voltage_preprocessed_AnomProb=55,n:Current_preprocessed_AnomProb=55,n',  # <<< *** Change as needed, i.e. classification_name=failure_prob:Voltage_preprocessed_AnomProb=55,n:Current_preprocessed_AnomProb=55,n
   'array' : '0',  # leave as is
   'transformtype' : '', # Sets the model to: log-lin,lin-log,log-log
   'sendcoefto' : '',  # you can send coefficients to another topic for further processing -- MUST BE SET IN STEP 2
