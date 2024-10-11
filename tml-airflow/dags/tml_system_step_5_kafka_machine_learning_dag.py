d47af98d1841807bd9bba8e3251ffa69a69ad9e9
smaurice101 <smaurice101@email>
2024-10-11 01:30:05 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml_system_step_5_kafka_machine_learning_dag.py
+++ tml-airflow/dags/tml_system_step_5_kafka_machine_learning_dag.py
@@ -222,12 +222,14 @@ if __name__ == '__main__':
         HPDEHOST = sys.argv[5]
         HPDEPORT = sys.argv[6]
         
+        tsslogging.locallogs("INFO", "STEP 5: Machine learning started")
     
         while True:
          try:     
           performSupervisedMachineLearning()
           time.sleep(1)
          except Exception as e:
+          tsslogging.locallogs("ERROR", "STEP 5: Machine Learning DAG in {} {}".format(os.path.basename(__file__),e))
           tsslogging.tsslogit("Machine Learning DAG in {} {}".format(os.path.basename(__file__),e), "ERROR" )                     
           tsslogging.git_push("/{}".format(repo),"Entry from {}".format(os.path.basename(__file__)),"origin")    
           break
