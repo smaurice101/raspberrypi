01fe0f469bd02a23e7ddfd37de61905a6face014
smaurice101 <smaurice101@email>
2024-10-12 16:03:32 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml_system_step_1_getparams_dag.py
+++ tml-airflow/dags/tml_system_step_1_getparams_dag.py
@@ -252,7 +252,7 @@ def updateviperenv():
     if tmlgood==0:
         repo=tsslogging.getrepo()   
         tsslogging.locallogs("ERROR", "STEP 1: Network issue - cannot ping TML binaries.  Tried 5 times in {}".format(os.path.basename(__file__)))
-        tsslogging.tsslogit("Step 1: Network issue - cannot reach TML binaries on ports. In File {}".format(os.path.basename(__file__)), "ERROR" )             
+        tsslogging.tsslogit("Step 1: Network issue - cannot reach TML binaries on visualization port. In File {}".format(os.path.basename(__file__)), "ERROR" )             
         tsslogging.git_push("/{}".format(repo),"Entry from {}".format(os.path.basename(__file__)),"origin")            
     time.sleep(3)        
     
