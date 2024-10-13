3d0ac75736c4e1ab501ee2833130f587c479b9c7
smaurice101 <smaurice101@email>
2024-10-13 02:15:22 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/realtimeiotdataprocessing-with-ml-3f10/tml_system_step_5_kafka_machine_learning_dag-realtimeiotdataprocessing-with-ml-3f10.py
+++ tml-airflow/dags/tml-solutions/realtimeiotdataprocessing-with-ml-3f10/tml_system_step_5_kafka_machine_learning_dag-realtimeiotdataprocessing-with-ml-3f10.py
@@ -19,7 +19,7 @@ default_args = {
   'enabletls': '1',   # <<< *** 1=connection is encrypted, 0=no encryption
   'microserviceid' : '', # <<< *** leave blank
   'producerid' : 'iotsolution',    # <<< *** Change as needed   
-  'preprocess_data_topic' : 'iot-preprocess', # << *** topic/data to use for training datasets - You created this in STEP 2
+  'preprocess_data_topic' : 'iot-preprocess2', # << *** topic/data to use for training datasets - You created this in STEP 2
   'ml_data_topic' : 'ml-data', # topic to store the trained algorithms  - You created this in STEP 2
   'identifier' : 'TML solution',    # <<< *** Change as needed   
   'companyname' : 'Your company', # <<< *** Change as needed      
