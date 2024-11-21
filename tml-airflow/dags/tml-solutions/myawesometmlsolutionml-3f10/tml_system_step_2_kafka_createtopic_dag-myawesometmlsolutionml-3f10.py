c1fa661eb9c757eb2acf50ade1ae9eb95e7682dd
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-11-21 08:27:59 -0500

    Update tml_system_step_2_kafka_createtopic_dag-myawesometmlsolutionml-3f10.py

--- tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tml_system_step_2_kafka_createtopic_dag-myawesometmlsolutionml-3f10.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tml_system_step_2_kafka_createtopic_dag-myawesometmlsolutionml-3f10.py
@@ -147,5 +147,4 @@ def setupkafkatopics(**context):
        tsslogging.tsslogit("Cannot create topic {} in {} - {}".format(topic,os.path.basename(__file__),e), "ERROR" )                     
        tsslogging.git_push("/{}".format(repo),"Entry from {}".format(os.path.basename(__file__)),"origin")  
         
-      print("Result=",result)
   tsslogging.locallogs("INFO", "STEP 2: Completed")
