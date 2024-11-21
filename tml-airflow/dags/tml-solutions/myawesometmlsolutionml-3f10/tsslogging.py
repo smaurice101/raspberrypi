87ffa043fa631f5a742e64f662f383a7eef80b40
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-11-20 20:55:25 -0500

    Update tsslogging.py

--- tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tsslogging.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tsslogging.py
@@ -1,5 +1,7 @@
 # TSS Logging
 
+# TSS Logging
+
 import datetime
 from datetime import timezone 
 from git import Repo
@@ -8,6 +10,137 @@ import subprocess
 import os
 import socket
 
+def genkubeyaml(sname,containername,clientport,solutionairflowport,solutionvipervizport,solutionexternalport,sdag,guser,grepo,chip,dockerusername,externalport,kuser,mqttuser,airflowport,vipervizport):
+    cp = ""
+    cpp = ""
+    if len(clientport) > 1:
+        cp = "        - containerPort: {}" \
+"        - containerPort: {}" \
+"        - containerPort: {}" \
+"        - containerPort: {}".format(clientport,solutionairflowport,solutionvipervizport,solutionexternalport)
+        cpp = clientport
+        cs="" \
+"  - port: {}" \
+"    name: p1 " \
+"    protocol: TCP" \
+"    targetPort: {}" \
+"  - port: {}" \
+"    name: p2" \
+"    protocol: TCP" \
+"    targetPort: {}" \
+"  - port: {}" \
+"    name: p3" \
+"    protocol: TCP" \
+"    targetPort: {}" \
+"  - port: {}" \
+"    name: p4" \
+"    protocol: TCP" \
+"    targetPort: {}".format(clientport,clientport,solutionairflowport,solutionairflowport,solutionvipervizport,solutionvipervizport,solutionexternalport,solutionexternalport)
+        
+    else:    
+        cp = "        - containerPort: {}" \
+"        - containerPort: {}" \
+"        - containerPort: {}".format(solutionexternalport,solutionairflowport,solutionvipervizport)
+        cpp = "0"
+        cs="" \
+"  - port: {}" \
+"    name: p2" \
+"    protocol: TCP" \
+"    targetPort: {}" \
+"  - port: {}" \
+"    name: p3" \
+"    protocol: TCP" \
+"    targetPort: {}" \
+"  - port: {}" \
+"    name: p4" \
+"    protocol: TCP" \
+"    targetPort: {}".format(solutionairflowport,solutionairflowport,solutionvipervizport,solutionvipervizport,solutionexternalport,solutionexternalport)
+        
+    kcmd="apiVersion: apps/v1 " \
+"kind: Deployment" \
+"metadata:" \
+"  name: {}" \
+"spec:" \
+"  selector:" \
+"    matchLabels:" \
+"      app: {}" \
+"  replicas: 3 # tells deployment to run 1 pods matching the template" \
+"  template:" \
+"    metadata:" \
+"      labels:" \
+"        app: {}" \
+"    spec:" \
+"      containers:" \
+"      - name: {}" \
+"        image: {}" \
+"        volumeMounts:" \
+"        - name: dockerpath" \
+"          mountPath: /var/run/docker.sock" \
+"        ports:" \
+"{}" \
+"        env:" \
+"        - name: TSS" \
+"          value: '0'" \
+"        - name: SOLUTIONNAME" \
+"          value: '{}'" \
+"        - name: SOLUTIONDAG" \
+"          value: '{}'" \
+"        - name: GITUSERNAME" \
+"          value: '{}'" \
+"        - name: GITREPOURL" \
+"          value: '{}'" \
+"        - name: SOLUTIONEXTERNALPORT" \
+"          value: '{}'" \
+"        - name: CHIP" \
+"          value: '{}'" \
+"        - name: SOLUTIONAIRFLOWPORT" \
+"          value: '{}'" \
+"        - name: SOLUTIONVIPERVIZPORT" \
+"          value: '{}'" \
+"        - name: DOCKERUSERNAME" \
+"          value: '{}'" \
+"        - name: CLIENTPORT" \
+"          value: '{}" \
+"        - name: EXTERNALPORT" \
+"          value: '{}'" \
+"        - name: KAFKACLOUDUSERNAME" \
+"          value: '{}'" \
+"        - name: VIPERVIZPORT" \
+"          value: '{}'" \
+"        - name: MQTTUSERNAME" \
+"          value: '{}'" \
+"        - name: AIRFLOWPORT" \
+"          value: '{}'" \
+"        - name: GITPASSWORD" \
+"          value: '<ENTER GITHUB PASSWORD>'" \
+"        - name: KAFKACLOUDPASSWORD" \
+"          value: '<Enter API secret>'" \
+"        - name: MQTTPASSWORD" \
+"          value: '<ENTER MQTT PASSWORD>'" \
+"        - name: READTHEDOCS" \
+"          value: '<ENTER READTHEDOCS TOKEN>'" \
+"        - name: KUBE" \
+"          value: '1'" \
+"      volumes: " \
+"      - name: dockerpath "\
+"        hostPath:" \
+"          path: /var/run/docker.sock" \
+"---" \
+"apiVersion: v1" \
+"kind: Service" \
+"metadata:" \
+"  name: {}" \
+"  labels:" \
+"    app: {}" \
+"spec:"\
+"  type: NodePort #Exposes the service as a node ports" \
+"  ports:" \
+"{}" \
+"  selector:" \
+"    app: cybersecuritywithprivategpt-3f10".format(sname,sname,sname,sname,containername,cp,sname,sdag,guser,grepo,solutionexternalport,chip,solutionairflowport,solutionvipervizport,dockerusername,cpp,externalport,kuser,vipervizport,mqttuser,airflowport,sname,sname,cs)  
+
+    return kcmd
+    
 def testvizconnection(portnum):
    good = 1
    #subprocess.call("curl localhost:{} &> /tmux/c.txt".format(portnum), shell=True)
