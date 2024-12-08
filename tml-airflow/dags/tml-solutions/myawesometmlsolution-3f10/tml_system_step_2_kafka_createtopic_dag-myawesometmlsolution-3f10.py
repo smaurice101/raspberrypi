c7a585ef7843ebef26775590fcec44ffa29b447f
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-12-08 12:19:50 -0500

    Update tml_system_step_2_kafka_createtopic_dag-myawesometmlsolution-3f10.py

--- tml-airflow/dags/tml-solutions/myawesometmlsolution-3f10/tml_system_step_2_kafka_createtopic_dag-myawesometmlsolution-3f10.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolution-3f10/tml_system_step_2_kafka_createtopic_dag-myawesometmlsolution-3f10.py
@@ -42,6 +42,9 @@ def startkafkasetup():
 dag = startkafkasetup()
 
 def deletetopics(topic):
+    if 'KUBE' in os.environ:
+       if os.environ['KUBE'] == "1":
+         return
     
     buf = "/Kafka/kafka_2.13-3.0.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --topic {} --delete".format(topic)
     res=subprocess.call(buf, shell=True)
