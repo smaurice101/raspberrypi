0f83b389cbcfa47746dfb9d0c85ca6e1782a1822
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-12-19 09:58:27 -0500

    Update tml_read_gRPC_step_3_kafka_producetotopic_dag-iotsolution-3f10.py

--- tml-airflow/dags/tml-solutions/iotsolution-3f10/tml_read_gRPC_step_3_kafka_producetotopic_dag-iotsolution-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolution-3f10/tml_read_gRPC_step_3_kafka_producetotopic_dag-iotsolution-3f10.py
@@ -43,7 +43,7 @@ default_args = {
 ######################################## DO NOT MODIFY BELOW #############################################
 
 # Instantiate your DAG
-@dag(dag_id="tml_read_gRPC_step_3_kafka_producetotopic_dag_cybersecuritywithprivategpt-3f10", default_args=default_args, tags=["tml_read_gRPC_step_3_kafka_producetotopic_dag_cybersecuritywithprivategpt-3f10"], schedule=None,catchup=False)
+@dag(dag_id="tml_read_gRPC_step_3_kafka_producetotopic_dag_iotsolution-3f10", default_args=default_args, tags=["tml_read_gRPC_step_3_kafka_producetotopic_dag_iotsolution-3f10"], schedule=None,catchup=False)
 def startproducingtotopic():
   # This sets the lat/longs for the IoT devices so it can be map
   def empty():
