b011f4a5e1a712e595d77061d617c7e87715a309
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-12-22 15:11:18 -0500

    Update tsslogging.py

--- tml-airflow/dags/tsslogging.py
+++ tml-airflow/dags/tsslogging.py
@@ -534,6 +534,7 @@ def optimizecontainer(cname,sname):
 
     i=0
     exists=0
+    ret=-1
     while True:
       i = i + 1  
       time.sleep(5)          
@@ -543,15 +544,20 @@ def optimizecontainer(cname,sname):
          break
         
       try:  
-        cname2="{}/{}-temp2".format(os.environ['DOCKERUSERNAME'], sname)  
+        cname2="{}/{}-temp2:squashed".format(os.environ['DOCKERUSERNAME'], sname)  
         ret=subprocess.check_output("docker ps -a | grep '{}' | wc -l".format(cname2), shell=True)        
+        ret=ret.decode("utf-8")
+        ret=ret.strip()
+        ret=int(ret)            
         if ret > 0:
           exists=1
         if (exists and ret==0):
           print("INFO: Container optimized")  
           break
-
+        
+        print("IN ret={},exists={}".format(ret,exists)) 
       except Exception as e:
+         print("ret={},exists={}".format(ret,exists)) 
          continue
             
     buf="docker image tag  {}sq:latest  {}".format(cname,cname)
