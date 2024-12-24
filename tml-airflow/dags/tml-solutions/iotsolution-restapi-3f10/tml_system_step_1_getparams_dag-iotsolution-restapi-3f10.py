4b88cc75bd602f391d1a4b15d34a180e66663de2
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-12-23 23:18:50 -0500

    Update tml_system_step_1_getparams_dag-iotsolution-restapi-3f10.py

--- tml-airflow/dags/tml-solutions/iotsolution-restapi-3f10/tml_system_step_1_getparams_dag-iotsolution-restapi-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolution-restapi-3f10/tml_system_step_1_getparams_dag-iotsolution-restapi-3f10.py
@@ -75,13 +75,6 @@ default_args = {
 }
 
 ############################################################### DO NOT MODIFY BELOW ####################################################
-# Instantiate your DAG
-@dag(dag_id="tml_system_step_1_getparams_dag_iotsolution-restapi-3f10", default_args=default_args, tags=["tml_system_step_1_getparams_dag_iotsolution-restapi-3f10"], schedule=None, catchup=False)
-def tmlparams():
-    # Define tasks
-    def empty():
-        pass
-dag = tmlparams()
 
     
 def reinitbinaries(sname):  
