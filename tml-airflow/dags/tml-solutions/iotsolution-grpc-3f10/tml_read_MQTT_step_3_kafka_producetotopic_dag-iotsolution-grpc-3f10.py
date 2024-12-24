1beb3736b145d8ac79528d51d9f2078d0c819f14
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-12-23 23:14:07 -0500

    Update tml_read_MQTT_step_3_kafka_producetotopic_dag-iotsolution-grpc-3f10.py

--- tml-airflow/dags/tml-solutions/iotsolution-grpc-3f10/tml_read_MQTT_step_3_kafka_producetotopic_dag-iotsolution-grpc-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolution-grpc-3f10/tml_read_MQTT_step_3_kafka_producetotopic_dag-iotsolution-grpc-3f10.py
@@ -39,13 +39,6 @@ default_args = {
 }
 
 ######################################## DO NOT MODIFY BELOW #############################################
-
-# Instantiate your DAG
-@dag(dag_id="tml_mqtt_step_3_kafka_producetotopic_dag_iotsolution-grpc-3f10", default_args=default_args, tags=["tml_mqtt_step_3_kafka_producetotopic_dag_iotsolution-grpc-3f10"], schedule=None,catchup=False)
-def startproducingtotopic():
-  def empty():
-    pass
-dag = startproducingtotopic()
     
 # This sets the lat/longs for the IoT devices so it can be map
 VIPERTOKEN=""
