916bcc344ac2f8a0e8a975127de3036253ed4bd9
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-12-24 12:47:18 -0500

    Update tml_client_MQTT_step_3_kafka_producetotopic.py

--- tml-airflow/dags/tml-solutions/iotsolution-restapi-3f10/tml_client_MQTT_step_3_kafka_producetotopic.py
+++ tml-airflow/dags/tml-solutions/iotsolution-restapi-3f10/tml_client_MQTT_step_3_kafka_producetotopic.py
@@ -79,6 +79,7 @@ def readdatafile(client,inputfile):
       time.sleep(.15)
       pass
 
-client=mqttconnection()
-inputfile = "IoTDatasample.txt"
-readdatafile(client,inputfile)
+#uncomment for MQTT client connection
+#client=mqttconnection()
+#inputfile = "IoTDatasample.txt"
+#readdatafile(client,inputfile)
