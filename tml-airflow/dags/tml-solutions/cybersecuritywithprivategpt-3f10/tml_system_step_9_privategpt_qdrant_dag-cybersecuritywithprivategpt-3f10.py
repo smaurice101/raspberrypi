8792c8f41d63d4cdb385e637b525a237ddc38f33
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-01-14 10:56:48 -0500

    Update tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
@@ -38,7 +38,7 @@ default_args = {
  'prompt': 'Do the anomaly probabilites show any risk of a cyber attack?', # Enter your prompt here
  'context' : 'This is network data from inbound and outbound packets. The data are \
 anomaly probabilities for cyber threats from analysis of inbound and outbound packets. If inbound or outbound \
-anomaly probabilities are less than 0.60, it is likely the risk of a cyber attack is also low.', # what is this data about? Provide context to PrivateGPT
+anomaly probabilities are less than 0.60, it is likely the risk of a cyber attack is also low. If its above 0.60, then risk is mid to high.', # what is this data about? Provide context to PrivateGPT
  'jsonkeytogather' : 'hyperprediction', # enter key you want to gather data from to analyse with PrivateGpt i.e. Identifier or hyperprediction
  'keyattribute' : 'inboundpackets,outboundpackets', # change as needed  
  'keyprocesstype' : 'anomprob',  # change as needed
