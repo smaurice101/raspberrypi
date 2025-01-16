0dccfbf624d17ba793af42a67887338b047f37f2
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-01-16 12:14:53 -0500

    Update tml_system_step_9_privategpt_qdrant_dag.py

--- tml-airflow/dags/tml_system_step_9_privategpt_qdrant_dag.py
+++ tml-airflow/dags/tml_system_step_9_privategpt_qdrant_dag.py
@@ -35,7 +35,7 @@ default_args = {
  'pgptport' : '8001', # PrivateGPT listening on this port
  'preprocesstype' : '', # Leave as is 
  'partition' : '-1', # Leave as is 
- 'prompt': 'Do the anomaly probabilites show any risk of a cyber attack?', # Enter your prompt here
+ 'prompt': 'Do the anomaly probabilities show any risk of a cyber attack?', # Enter your prompt here
  'context' : 'This is network data from inbound and outbound packets. The data are \
 anomaly probabilities for cyber threats from analysis of inbound and outbound packets. If inbound or outbound \
 anomaly probabilities are less than 0.60, it is likely the risk of a cyber attack is also low. If its above 0.60, then risk is mid to high.', # what is this data about? Provide context to PrivateGPT
