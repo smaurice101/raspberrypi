3cd10fb3e39ca4ef22910b3959b2586b88963f4f
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-01-17 18:35:20 -0500

    Update tml_system_step_6_kafka_predictions_dag.py

--- tml-airflow/dags/tml_system_step_6_kafka_predictions_dag.py
+++ tml-airflow/dags/tml_system_step_6_kafka_predictions_dag.py
@@ -182,7 +182,7 @@ def startpredictions(**context):
        wn = windowname('predict',sname,sd)     
        subprocess.run(["tmux", "new", "-d", "-s", "{}".format(wn)])
        subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "cd /Viper-predict", "ENTER"])
-       subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "python {} 1 {} {}{} {} {}{} {}".format(fullpath,VIPERTOKEN,HTTPADDR,VIPERHOST,VIPERPORT[1:],HPDEADDR,HPDEHOSTPREDICT,HPDEPORTPREDICT[1:]), "ENTER"])        
+       subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "python {} 1 {} {}{} {} {}{} {} {}".format(fullpath,VIPERTOKEN,HTTPADDR,VIPERHOST,VIPERPORT[1:],HPDEADDR,HPDEHOSTPREDICT,HPDEPORTPREDICT[1:],context), "ENTER"])        
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -201,6 +201,12 @@ if __name__ == '__main__':
          VIPERPORT=sys.argv[4]
          HPDEHOSTPREDICT=sys.argv[5]
          HPDEPORTPREDICT=sys.argv[6]    
+         context =  sys.argv[7]
+         sd = context['dag'].dag_id
+         sname=context['ti'].xcom_pull(task_ids='step_1_solution_task_getparams',key="{}_solutionname".format(sd))         
+         maxrows = context['ti'].xcom_pull(task_ids='step_6_solution_task_prediction',key="{}_maxrows".format(sname))
+         default_args['maxrows'] = maxrows[1:]
+         
          tsslogging.locallogs("INFO", "STEP 6: Predictions started")
          while True:
           try:              
