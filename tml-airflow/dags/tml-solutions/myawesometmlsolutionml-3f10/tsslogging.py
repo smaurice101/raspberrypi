28a0d9f9146e39713b87e6b920235eefb06ee21d
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-01-29 19:15:19 -0500

    Update tsslogging.py

--- tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tsslogging.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tsslogging.py
@@ -180,7 +180,7 @@ def genkubeyaml(sname,containername,clientport,solutionairflowport,solutionviper
                step9rollbackoffset,kubebroker,kafkabroker,producetype,step9prompt='',step9context='',step9keyattribute='',step9keyprocesstype='',
                step9hyperbatch='',step9vectordbcollectionname='',step9concurrency='',cudavisibledevices='',step9docfolder='',
                step9docfolderingestinterval='',step9useidentifierinprompt='',step5processlogic='',step5independentvariables='',
-               step9searchterms='',step9streamall='',step9temperature='',step9vectorsearchtype=''):
+               step9searchterms='',step9streamall='',step9temperature='',step9vectorsearchtype='',step9llmmodel='',step9embedding='',step9vectorsize=''):
                
     cp = ""
     cpp = ""
@@ -354,6 +354,12 @@ def genkubeyaml(sname,containername,clientport,solutionairflowport,solutionviper
                value: '{}'                                             
              - name: step9vectorsearchtype # privateGPT for QDrant VectorDB similarity search.  Must be either Cosine, Manhattan, Dot, Euclid
                value: '{}'                                                            
+             - name: llmmodel # privateGPT LLM model used
+               value: '{}'                                                            
+             - name: embedding # privateGPT Embedding for LLM model
+               value: '{}'                                                                 
+             - name: embeddingvectorsize # privateGPT Embedding vector size
+               value: '{}'                                                                                          
              - name: step1solutiontitle # STEP 1 solutiontitle field can be adjusted here. 
                value: '{}'                              
              - name: step1description # STEP 1 description field can be adjusted here. 
@@ -400,7 +406,8 @@ def genkubeyaml(sname,containername,clientport,solutionairflowport,solutionviper
          app: {}""".format(sname,sname,sname,sname,containername,cp,sname,sdag,guser,grepo,solutionexternalport,chip,solutionairflowport,solutionvipervizport,dockerusername,cpp,externalport,kuser,vipervizport,mqttuser,
                            airflowport,step4maxrows,step4bmaxrows,step5rollbackoffsets,step5processlogic,step5independentvariables,step6maxrows,step9rollbackoffset,
                            step9prompt,step9context,step9keyattribute,step9keyprocesstype,step9hyperbatch,step9vectordbcollectionname,step9concurrency,cudavisibledevices,
-                           step9docfolder,step9docfolderingestinterval,step9useidentifierinprompt,step9searchterms,step9streamall,step9temperature,step9vectorsearchtype,step1solutiontitle,step1description,kubebroker,kafkabroker,
+                           step9docfolder,step9docfolderingestinterval,step9useidentifierinprompt,step9searchterms,step9streamall,step9temperature,step9vectorsearchtype,
+                           step9llmmodel,step9embedding,step9vectorsize,step1solutiontitle,step1description,kubebroker,kafkabroker,
                            sname,sname,solutionvipervizport,sname,sname,sname,mport,cpp,sname)
                     
     return kcmd
@@ -411,7 +418,7 @@ def genkubeyamlnoext(sname,containername,clientport,solutionairflowport,solution
                      step9rollbackoffset,kubebroker,kafkabroker,step9prompt='',step9context='',step9keyattribute='',step9keyprocesstype='',
                      step9hyperbatch='',step9vectordbcollectionname='',step9concurrency='',cudavisibledevices='',step9docfolder='',
                      step9docfolderingestinterval='',step9useidentifierinprompt='',step5processlogic='',step5independentvariables='',
-                     step9searchterms='',step9streamall='',step9temperature='',step9vectorsearchtype=''):
+                     step9searchterms='',step9streamall='',step9temperature='',step9vectorsearchtype='',step9llmmodel='',step9embedding='',step9vectorsize=''):
     cp = ""
     cpp = ""
     
@@ -580,6 +587,12 @@ def genkubeyamlnoext(sname,containername,clientport,solutionairflowport,solution
                value: '{}'                                             
              - name: step9vectorsearchtype # privateGPT for QDrant VectorDB similarity search.  Must be either Cosine, Manhattan, Dot, Euclid
                value: '{}'                                                                           
+             - name: llmmodel # privateGPT LLM model used
+               value: '{}'                                                            
+             - name: embedding # privateGPT Embedding for LLM model
+               value: '{}'                                                                 
+             - name: embeddingvectorsize # privateGPT Embedding vector size
+               value: '{}'                                                                                                         
              - name: step1solutiontitle # STEP 1 solutiontitle field can be adjusted here. 
                value: '{}'                              
              - name: step1description # STEP 1 description field can be adjusted here. 
@@ -610,7 +623,8 @@ def genkubeyamlnoext(sname,containername,clientport,solutionairflowport,solution
          app: {}""".format(sname,sname,sname,sname,containername,cp,sname,sdag,guser,grepo,solutionexternalport,chip,solutionairflowport,solutionvipervizport,dockerusername,cpp,externalport,kuser,vipervizport,
                            mqttuser,airflowport,step4maxrows,step4bmaxrows,step5rollbackoffsets,step5processlogic,step5independentvariables,step6maxrows,step9rollbackoffset,
                            step9prompt,step9context,step9keyattribute,step9keyprocesstype,step9hyperbatch,step9vectordbcollectionname,step9concurrency,cudavisibledevices,
-                           step9docfolder,step9docfolderingestinterval,step9useidentifierinprompt,step9searchterms,step9streamall,step9temperature,step9vectorsearchtype,step1solutiontitle,step1description,kubebroker,kafkabroker,
+                           step9docfolder,step9docfolderingestinterval,step9useidentifierinprompt,step9searchterms,step9streamall,step9temperature,step9vectorsearchtype,
+                           step9llmmodel,step9embedding,step9vectorsize,step1solutiontitle,step1description,kubebroker,kafkabroker,
                            sname,sname,solutionvipervizport,sname)
                     
     return kcmd
