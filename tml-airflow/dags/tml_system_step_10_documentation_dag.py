c26837f7fa7934352249e3114fa7a0e05c616b62
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-02-10 22:16:39 -0500

    Update tml_system_step_10_documentation_dag.py

--- tml-airflow/dags/tml_system_step_10_documentation_dag.py
+++ tml-airflow/dags/tml_system_step_10_documentation_dag.py
@@ -170,7 +170,7 @@ def generatedoc(**context):
  
     projecturl="https://github.com/{}/{}/tree/main/tml-airflow/dags/tml-solutions/{}".format(os.environ['GITUSERNAME'],tsslogging.getrepo(),projectname)
  
-    doparse("/{}/docs/source/index.rst".format(sname), ["--projectname--{}".format(projectname)])
+    doparse("/{}/docs/source/index.rst".format(sname), ["--projectname--;{}".format(projectname)])
 
     subprocess.call(["sed", "-i", "-e",  "s/--solutionname--/{}/g".format(sname), "/{}/docs/source/details.rst".format(sname)])
     subprocess.call(["sed", "-i", "-e",  "s/--sname--/{}/g".format(sname), "/{}/docs/source/details.rst".format(sname)])
