e0cac3e585e01de9848722a730a7bd442e5ba435
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-02-10 19:27:49 -0500

    Update tml_system_step_1_getparams_dag.py

--- tml-airflow/dags/tml_system_step_1_getparams_dag.py
+++ tml-airflow/dags/tml_system_step_1_getparams_dag.py
@@ -471,6 +471,7 @@ def getparams(**context):
   task_instance.xcom_push(key="{}_HPDEHOSTPREDICT".format(sname),value=HPDEHOSTPREDICT)
   task_instance.xcom_push(key="{}_HPDEPORTPREDICT".format(sname),value="_{}".format(HPDEPORTPREDICT))
   task_instance.xcom_push(key="{}_solutionname".format(sd),value=sname)
+  task_instance.xcom_push(key="{}_projectname".format(sd),value=sname)
   task_instance.xcom_push(key="{}_solutiondescription".format(sname),value=desc)
   task_instance.xcom_push(key="{}_solutiontitle".format(sname),value=stitle)
 
