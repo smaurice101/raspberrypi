ecd5e599c4ab39274239a0cd650fd6122540f7e3
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-02-13 16:33:52 -0500

    Update tml_system_step_7_kafka_visualization_dag.py

--- tml-airflow/dags/tml_system_step_7_kafka_visualization_dag.py
+++ tml-airflow/dags/tml_system_step_7_kafka_visualization_dag.py
@@ -20,6 +20,8 @@ default_args = {
   'topic-ml' : 'iot-preprocess,iot-preprocess2',    # <<< *** Separate multiple topics by a comma
   'dashboardhtml-ai': 'dashboard-ai.html', # <<< *** This one is you include AI dag
   'topic-ai' : 'iot-preprocess,iot-preprocess2',    # <<< *** Separate multiple topics by a comma  
+  'dashboardhtml-ml-ai': 'dashboard-ml-ai.html', # <<< *** This one is you include ML-AI dag
+  'topic-ml-ai' : 'iot-preprocess,iot-preprocess2',    # <<< *** Separate multiple topics by a comma    
   'secure': '1',   # <<< *** 1=connection is encrypted, 0=no encryption
   'offset' : '-1',    # <<< *** -1 indicates to read from the last offset always
   'append' : '0',   # << ** Do not append new data in the browser
@@ -54,7 +56,10 @@ def startstreamingengine(**context):
         solutionvipervizport = context['ti'].xcom_pull(task_ids='step_1_solution_task_getparams',key="{}_SOLUTIONVIPERVIZPORT".format(sname)) 
         tss = context['ti'].xcom_pull(task_ids='step_1_solution_task_getparams',key="{}_TSS".format(sname)) 
 
-        if '_ai_' in sd:
+        if '_ml_ai_' in sd:
+          topic = default_args['topic-ai']
+          dashboardhtml = default_args['dashboardhtml-ai']  
+        elif '_ai_' in sd:
           topic = default_args['topic-ai']
           dashboardhtml = default_args['dashboardhtml-ai']
         elif '_ml_' in sd:  
