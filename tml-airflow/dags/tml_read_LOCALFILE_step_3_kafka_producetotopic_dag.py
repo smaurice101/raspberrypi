d28e70def6cc71bacdcf0c8bb0a0471360babe58
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-02-15 18:17:14 -0500

    Update tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py

--- tml-airflow/dags/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py
+++ tml-airflow/dags/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py
@@ -218,6 +218,12 @@ def startproducing(**context):
     
   ti.xcom_push(key="{}_PORT".format(sname),value="_{}".format(VIPERPORT))
   ti.xcom_push(key="{}_HTTPADDR".format(sname),value=HTTPADDR)
+
+  if 'docfolder' in default_args and 'doctopic' in default_args:
+    ti.xcom_push(key="{}_docfolder".format(sname),value=default_args['docfolder'])
+    ti.xcom_push(key="{}_doctopic".format(sname),value=default_args['doctopic'])
+    ti.xcom_push(key="{}_chunks".format(sname),value="_{}".format(default_args['chunks']))
+    ti.xcom_push(key="{}_docingestinterval".format(sname),value=default_args['docingestinterval'])
         
   chip = context['ti'].xcom_pull(task_ids='step_1_solution_task_getparams',key="{}_chip".format(sname))   
 
