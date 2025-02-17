59c2f083c37a074d1b7eb01cbea0c74b14c2dcdd
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-02-17 12:50:06 -0500

    Update tml_system_step_4_kafka_preprocess_dag-cybersecuritywithprivategpt-3f10.py

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_4_kafka_preprocess_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_4_kafka_preprocess_dag-cybersecuritywithprivategpt-3f10.py
@@ -18,9 +18,9 @@ default_args = {
   'owner' : 'Sebastian Maurice',  # <<< *** Change as needed      
   'enabletls': '1', # <<< *** 1=connection is encrypted, 0=no encryption
   'microserviceid' : '',  # <<< *** leave blank
-  'producerid' : 'cybersecurity',   # <<< *** Change as needed   
-  'raw_data_topic' : 'cisco-network-mainstream', # *************** INCLUDE ONLY ONE TOPIC - This is one of the topic you created in SYSTEM STEP 2
-  'preprocess_data_topic' : 'cisco-network-preprocess', # *************** INCLUDE ONLY ONE TOPIC - This is one of the topic you created in SYSTEM STEP 2
+  'producerid' : 'iotsolution',   # <<< *** Change as needed   
+  'raw_data_topic' : 'iot-raw-data', # *************** INCLUDE ONLY ONE TOPIC - This is one of the topic you created in SYSTEM STEP 2
+  'preprocess_data_topic' : 'iot-preprocess', # *************** INCLUDE ONLY ONE TOPIC - This is one of the topic you created in SYSTEM STEP 2
   'maxrows' : '800', # <<< ********** Number of offsets to rollback the data stream -i.e. rollback stream by 500 offsets
   'offset' : '-1', # <<< Rollback from the end of the data streams  
   'brokerhost' : '',   # <<< *** Leave as is
@@ -36,17 +36,18 @@ default_args = {
   'tmlfilepath' : '', # leave blank
   'usemysql' : '1', # do not modify
   'streamstojoin' : '', # leave blank
-  'identifier' : 'Cybersecurity Anomaly detection', # <<< ** Change as needed
+  'identifier' : 'IoT device performance and failures', # <<< ** Change as needed
   'preprocesstypes' : 'anomprob,trend,avg', # <<< **** MAIN PREPROCESS TYPES CHNAGE AS NEEDED refer to https://tml-readthedocs.readthedocs.io/en/latest/
   'pathtotmlattrs' : 'oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a', # Change as needed     
-  'jsoncriteria' : 'uid=hostName,filter:allrecords~\
-subtopics=hostName,hostName,hostName~\
-values=inboundpackets,outboundpackets,pingStatus~\
-identifiers=inboundpackets,outboundpackets,pingStatus~\
-datetime=lastUpdated~\
-msgid=~\
-latlong='
+  'jsoncriteria' : 'uid=metadata.dsn,filter:allrecords~\
+subtopics=metadata.property_name~\
+values=datapoint.value~\
+identifiers=metadata.display_name~\
+datetime=datapoint.updated_at~\
+msgid=datapoint.id~\
+latlong=lat:long' # <<< **** Specify your json criteria. Here is an example of a multiline json --  refer to https://tml-readthedocs.readthedocs.io/en/latest/
 }
+
 ######################################## DO NOT MODIFY BELOW #############################################
 
 VIPERTOKEN=""
@@ -115,10 +116,7 @@ def processtransactiondata():
 
  # if dataage - use:dataage_utcoffset_timetype
  preprocesstypes=default_args['preprocesstypes']
-
  pathtotmlattrs=default_args['pathtotmlattrs']       
- raw_data_topic = default_args['raw_data_topic']  
- preprocess_data_topic = default_args['preprocess_data_topic']  
     
  try:
     result=maadstml.viperpreprocesscustomjson(VIPERTOKEN,VIPERHOST,VIPERPORT,topic,producerid,offset,jsoncriteria,rawdataoutput,maxrows,enabletls,delay,brokerhost,
