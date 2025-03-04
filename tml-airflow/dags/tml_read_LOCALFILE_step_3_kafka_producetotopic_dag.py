48464100f8a55e98319bb479c36c9457446d43ec
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-03-04 14:00:29 -0500

    Update tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py

--- tml-airflow/dags/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py
+++ tml-airflow/dags/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py
@@ -94,7 +94,7 @@ def ingestfiles():
       if len(dirbuf) != len(maintopicbuf):
         tsslogging.locallogs("ERROR", "STEP 3: Produce LOCALFILE in {} You specified multiple doctopics, then must match docfolder".format(os.path.basename(__file__)))
         return
-    elif len(maintopicbuf) == 1 and len(dirbuf) > 0:
+    elif len(maintopicbuf) == 1 and len(dirbuf) > 1:
        for i in range(len(dirbuf)-1):
          maintopicbuf.append(maintopic)
     else:
