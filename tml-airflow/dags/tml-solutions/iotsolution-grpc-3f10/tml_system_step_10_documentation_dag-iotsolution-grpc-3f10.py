5578fa7526e281bf15815f317392ab141e20605f
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-03-17 16:33:06 -0400

    Update tml_system_step_10_documentation_dag-iotsolution-grpc-3f10.py

--- tml-airflow/dags/tml-solutions/iotsolution-grpc-3f10/tml_system_step_10_documentation_dag-iotsolution-grpc-3f10.py
+++ tml-airflow/dags/tml-solutions/iotsolution-grpc-3f10/tml_system_step_10_documentation_dag-iotsolution-grpc-3f10.py
@@ -20,7 +20,9 @@ default_args = {
  'conf_copyright' : '2024, Otics Advanced Analytics, Incorporated - For Support email support@otics.ca',
  'conf_author' : 'Sebastian Maurice',
  'conf_release' : '0.1',
- 'conf_version' : '0.1.0'
+ 'conf_version' : '0.1.0',
+ 'dockerenv': '', # add any environmental variables for docker must be: variable1=value1, variable2=value2
+ 'dockerinstructions': '', # add instructions on how to run the docker container  
 }
 
 ############################################################### DO NOT MODIFY BELOW ####################################################
