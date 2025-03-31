f5b0dcf16995970bf258bdb86d9c96ea406d0cfb
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-03-30 22:33:37 -0400

    Update tml_system_step_4a_kafka_preprocess_dag.py

--- tml-airflow/dags/tml_system_step_4a_kafka_preprocess_dag.py
+++ tml-airflow/dags/tml_system_step_4a_kafka_preprocess_dag.py
@@ -180,7 +180,7 @@ def dopreprocessing(**context):
        else:
          fullpath="/{}/tml-airflow/dags/{}".format(repo,os.path.basename(__file__))  
             
-       wn = windowname('preprocess',sname,sd)     
+       wn = windowname('preprocess1',sname,sd)     
        subprocess.run(["tmux", "new", "-d", "-s", "{}".format(wn)])
        subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "cd /Viper-preprocess2", "ENTER"])
        subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "python {} 1 {} {}{} {} {}".format(fullpath,VIPERTOKEN,HTTPADDR,VIPERHOST,VIPERPORT[1:],maxrows), "ENTER"])        
