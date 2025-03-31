2cfcb8a30b3d0954b0c504c717a9a1968515c0a9
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-03-30 21:28:21 -0400

    Update tml_system_step_4a_kafka_preprocess_dag.py

--- tml-airflow/dags/tml_system_step_4a_kafka_preprocess_dag.py
+++ tml-airflow/dags/tml_system_step_4a_kafka_preprocess_dag.py
@@ -182,7 +182,7 @@ def dopreprocessing(**context):
             
        wn = windowname('preprocess',sname,sd)     
        subprocess.run(["tmux", "new", "-d", "-s", "{}".format(wn)])
-       subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "cd /Viper-preprocess", "ENTER"])
+       subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "cd /Viper-preprocess2", "ENTER"])
        subprocess.run(["tmux", "send-keys", "-t", "{}".format(wn), "python {} 1 {} {}{} {} {}".format(fullpath,VIPERTOKEN,HTTPADDR,VIPERHOST,VIPERPORT[1:],maxrows), "ENTER"])        
 
 if __name__ == '__main__':
