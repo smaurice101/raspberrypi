4c4d9b091267c82f2ebfbb6e97bad0de255ae08b
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-04-10 11:39:04 -0400

    Update tml_system_step_10_documentation_dag.py

--- tml-airflow/dags/tml_system_step_10_documentation_dag.py
+++ tml-airflow/dags/tml_system_step_10_documentation_dag.py
@@ -196,11 +196,11 @@ def generatedoc(**context):
     step4braw_data_topic=""
     step4bpreprocess_data_topic=""
 
-    if "KUBE" in os.environ:
-          if os.environ["KUBE"] == "1":
-             kube=1
-             tsslogging.locallogs("INFO", "STEP 10: In Kubernetes documentation done")
-             return
+#    if "KUBE" in os.environ:
+#          if os.environ["KUBE"] == "1":
+#             kube=1
+#             tsslogging.locallogs("INFO", "STEP 10: In Kubernetes documentation done")
+#             return
     
     tsslogging.locallogs("INFO", "STEP 10: Started to build the documentation")
     producinghost = context['ti'].xcom_pull(task_ids='step_1_solution_task_getparams',key="{}_VIPERHOSTPRODCE".format(sname))
