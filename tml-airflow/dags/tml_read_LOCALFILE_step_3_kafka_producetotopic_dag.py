812e425509dac52e2129624458ad14deaf236a0f
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2026-05-29 13:52:32 -0400

    Update tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py

--- tml-airflow/dags/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py
+++ tml-airflow/dags/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag.py
@@ -25,13 +25,13 @@ default_args = {
   'producerid' : 'iotsolution',   # <<< *** Change as needed   
   'topics' : 'iot-raw-data', # *************** This is one of the topic you created in SYSTEM STEP 2
   'identifier' : 'TML solution',   # <<< *** Change as needed   
-  'inputfile' : '',#'/rawdatademo/cisco_network_data.txt',  # <<< ***** replace ?  to input file name to read. NOTE this data file should be JSON messages per line and stored in the HOST folder mapped to /rawdata folder 
+  'inputfile' : '/rawdatademo/IoTData.txt',  # <<< ***** replace ?  to input file name to read. NOTE this data file should be JSON messages per line and stored in the HOST folder mapped to /rawdata folder 
   'delay' : '7000', # << ******* 7000 millisecond maximum delay for VIPER to wait for Kafka to return confirmation message is received and written to topic
   'topicid' : '-999', # <<< ********* do not modify  
   'sleep' : 0.15, # << Control how fast data streams - if 0 - the data will stream as fast as possible - BUT this may cause connecion reset by peer 
-  'docfolder' : '/rawdatademo/mylogsdemo,/rawdatademo/mylogs2demo', # You can read TEXT files or any file in these folders that are inside the volume mapped to /rawdata
-  'doctopic' : 'rtms-stream-mylogs,rtms-stream-mylogs2',  # This is the topic that will contain the docfolder file data
-  'chunks' :3000, # if 0 the files in docfolder are read line by line, otherwise they are read by chunks i.e. 512
+  'docfolder' : '', # You can read TEXT files or any file in these folders that are inside the volume mapped to /rawdata
+  'doctopic' : '',  # This is the topic that will contain the docfolder file data
+  'chunks' : 0, # if 0 the files in docfolder are read line by line, otherwise they are read by chunks i.e. 512
   'docingestinterval' : 0, # specify the frequency in seconds to read files in docfolder - if 0 the files are read ONCE
 }
 
